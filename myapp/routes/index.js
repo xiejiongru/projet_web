424000b9db438886c9739696a0e05c920207641b
xiejiongru <xiejiongru@outlook.com>
2025-03-05 18:05:11 +0100

    test1

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { readSensorDataAndStore } = require('../recupdata'); // Corrige l'import
+// const { readSensorDataAndStore } = require('../recupdata'); // Corrige l'import
 
 const { InfluxDB } = require('@influxdata/influxdb-client');
 const influxConfig = {
